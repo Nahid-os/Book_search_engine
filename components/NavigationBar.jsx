76474b250d974e26050d049269cf8ff580404c44
Nahid <nahidhusen23@gmail.com>
2025-02-23 21:29:02 +0000

    add login/register links to navigation bar

--- components/NavigationBar.jsx
+++ components/NavigationBar.jsx
@@ -1,5 +1,7 @@
+// NavigationBar.jsx
 import React from 'react';
 import { Home, Search, BookmarkPlus, User, Sun, Moon } from 'lucide-react';
+import Link from 'next/link';
 
 const NavigationBar = ({ isDarkMode, toggleTheme, setIsSearchView }) => (
   <header className="bg-white dark:bg-gray-800 shadow-md transition-all duration-300">
@@ -20,9 +22,11 @@ const NavigationBar = ({ isDarkMode, toggleTheme, setIsSearchView }) => (
           <button onClick={toggleTheme} className="text-gray-600 hover:text-blue-600 dark:text-gray-300 dark:hover:text-blue-400 transition-colors duration-300">
             {isDarkMode ? <Sun className="h-5 w-5" /> : <Moon className="h-5 w-5" />}
           </button>
-          <button className="text-gray-600 hover:text-blue-600 dark:text-gray-300 dark:hover:text-blue-400 flex items-center transition-colors duration-300">
-            <User className="h-5 w-5 mr-2" /> Login / Sign Up
-          </button>
+          <Link href="/login">
+            <button className="text-gray-600 hover:text-blue-600 dark:text-gray-300 dark:hover:text-blue-400 flex items-center transition-colors duration-300">
+              <User className="h-5 w-5 mr-2" /> Login / Sign Up
+            </button>
+          </Link>
         </div>
       </nav>
     </div>
