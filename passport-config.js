f0ad88c8adb0b90f4787f4a7bb4dc0f5f7531915
Nahid <nahidhusen23@gmail.com>
2025-02-26 23:54:42 +0000

    serialize user ID as string and deserialize using ObjectId conversion

--- passport-config.js
+++ passport-config.js
@@ -1,10 +1,11 @@
 // passport-config.js
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt'); // or 'bcryptjs'
+const { ObjectId } = require('mongodb');
 
 function initializePassport(passport, getUserByEmail, getUserById) {
   const authenticateUser = async (email, password, done) => {
-    // use the email to find the user
+    // Use the email to find the user
     const user = await getUserByEmail(email);
     if (!user) {
       return done(null, false, { message: 'No user with that email' });
@@ -21,10 +22,18 @@ function initializePassport(passport, getUserByEmail, getUserById) {
   };
 
   passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser));
-  passport.serializeUser((user, done) => done(null, user._id));
+
+  // Serialize the user: store the _id as a string
+  passport.serializeUser((user, done) => done(null, user._id.toString()));
+
+  // Deserialize the user: convert the stored string back to an ObjectId before fetching the user
   passport.deserializeUser(async (id, done) => {
-    const user = await getUserById(id);
-    return done(null, user);
+    try {
+      const user = await getUserById(new ObjectId(id));
+      done(null, user);
+    } catch (err) {
+      done(err);
+    }
   });
 }
 
